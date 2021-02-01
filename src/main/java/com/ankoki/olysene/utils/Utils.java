6fd6158441a4d6d31714810f554b2ded3998e61e
Ankoki-Dev <ankoki.mc@gmail.com>
2021-02-01 01:24:45 +0000

    pop a choccy milk

--- src/main/java/com/ankoki/olysene/utils/Utils.java
+++ src/main/java/com/ankoki/olysene/utils/Utils.java
@@ -6,9 +6,7 @@ import org.bukkit.plugin.Plugin;
 import java.util.TreeMap;
 
 public final class Utils {
-    private Utils() {
-    }
-
+    private Utils() {}
     private static final TreeMap<Integer, String> rn = new TreeMap<>();
 
     public static String coloured(String uncoloured) {
@@ -24,20 +22,6 @@ public final class Utils {
         return builder.toString();
     }
 
-    /**
-     * Checks the version of a plugin compared too a version you want
-     * it to match up with, for example, if you wanted to make sure if a plugin
-     * had a version higher than 1.2, you can use versionChecker(Plugin, 1, 2);
-     *
-     * @param plugin Plugin is the plugin you want to check the version of, usually
-     *               obtained through Bukkit.getPluginManager.getPlugin("pluginName");
-     * @param major  The major of the version you want to check against, if there was
-     *               a version 1.2, 1 would be the major.
-     * @param minor  The minor of the version you want to check against, if there was
-     *               a version 1.2, 2 would be the minor.
-     * @return whether the plugins version is equal to or greater than the inputted
-     * version.
-     */
     public static boolean checkPluginVersion(Plugin plugin, int major, int minor) {
         major *= 10;
         int pluginVer = Integer.parseInt(plugin.getDescription().getVersion().replace(".", ""));
@@ -68,4 +52,18 @@ public final class Utils {
         return rn.get(l) + toRoman(number - l);
     }
 
+    public static boolean serverNewer(Version version, Version serverVersion) {
+        int sMajor = Integer.parseInt(serverVersion.name().split("_")[1]);
+        int sMinor = Integer.parseInt(serverVersion.name().split("_")[2].substring(1));
+
+        int vMajor = Integer.parseInt(version.toString().split("_")[1]);
+        int vMinor = Integer.parseInt(version.toString().split("_")[2].substring(1));
+
+        sMajor *= 10;
+        vMajor *= 10;
+        int sFin = sMajor + sMinor;
+        int vFin = vMajor + vMinor;
+
+        return sFin >= vFin;
+    }
 }

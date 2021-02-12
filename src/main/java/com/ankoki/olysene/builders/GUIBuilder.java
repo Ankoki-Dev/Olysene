e50b8639d1a4eb8dbeeb3bcdf62348fa83806017
Ankoki-Dev <ankoki.mc@gmail.com>
2021-02-12 00:33:56 +0000

    guibuilder start.

--- src/main/java/com/ankoki/olysene/builders/GUIBuilder.java
+++ src/main/java/com/ankoki/olysene/builders/GUIBuilder.java
@@ -5,6 +5,7 @@ import com.ankoki.olysene.utils.Utils;
 import com.ankoki.olysene.utils.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,37 +16,42 @@ public class GUIBuilder extends Builder<Inventory> {
     private final List<Inventory> pages = new ArrayList<>();
     private Material pageMaterial = null;
 
-    public GUIBuilder(int rows, String... name) {
+    public GUIBuilder(int rows, String name) {
         Validate.isGreater(rows, 9, "You cannot have an inventory with more than 9 rows!");
         Validate.isLess(rows, 2, "You cannot have an inventory with less than 2 rows!");
-        if (name == null) {
-            mainInv = Bukkit.createInventory(null, rows, "");
-        } else {
-            Validate.isLength(name, 1, "You can only put one name!");
-            String n = name[0];
-            mainInv = Bukkit.createInventory(null, rows, Utils.coloured(n));
-        }
+        mainInv = Bukkit.createInventory(null, rows, Utils.coloured(name));
     }
 
-    public void setItem(int slot, ItemStack item) {
+    public GUIBuilder(int rows, String name, InventoryType type) {
+        Validate.isGreater(rows, 9, "You cannot have an inventory with more than 9 rows!");
+        Validate.isLess(rows, 2, "You cannot have an inventory with less than 2 rows!");
+        mainInv = Bukkit.createInventory(null, type, Utils.coloured(name));
+    }
+
+    public GUIBuilder setItem(int slot, ItemStack item) {
         mainInv.setItem(slot, item);
+        return this;
     }
 
-    public void addItem(ItemStack item) {
+    public GUIBuilder addItem(ItemStack item) {
         mainInv.addItem(item);
+        return this;
     }
 
-    public void setPageMaterial(Material material) {
+    public GUIBuilder setPageMaterial(Material material) {
         Validate.notNull(material, "Page material cannot be null!");
         this.pageMaterial = material;
+        return this;
     }
 
-    public void setPage(int page, Inventory inventory) {
+    public GUIBuilder setPage(int page, Inventory inventory) {
         pages.set(page, inventory);
+        return this;
     }
 
-    public void addPage(Inventory inventory) {
+    public GUIBuilder addPage(Inventory inventory) {
         pages.add(inventory);
+        return this;
     }
 
     public Inventory getPage(int page) {
@@ -55,6 +61,43 @@ public class GUIBuilder extends Builder<Inventory> {
         } catch (ArrayIndexOutOfBoundsException ex) {
             throw new IllegalArgumentException("You cannot get a page which doesn't exist!");
         }
+    }
+
+    public GUIBuilder setBorderSlots(ItemStack item) {
+        for (int slot : getBorderSlots(this.mainInv)) {
+            mainInv.setItem(slot, item);
+        }
+        return this;
+    }
+
+    private Integer[] getBorderSlots(Inventory inv) {
+        List<Integer> slotsList = new ArrayList<>();
+        InventoryType type = inv.getType();
+        int rows = 0;
+        if (type == InventoryType.CHEST ||
+                type == InventoryType.ENDER_CHEST ||
+                type == InventoryType.SHULKER_BOX ||
+                type == InventoryType.BARREL) rows = inv.getSize() / 9;
+        if (type == InventoryType.DISPENSER ||
+                type == InventoryType.DROPPER) rows = 3;
+        if (type == InventoryType.HOPPER) return new Integer[]{0, 4};
+        if (rows == 0) return new Integer[]{};
+        int slots = inv.getSize();
+        int slotsPerRow = slots / rows;
+        for (int i = 1; i <= rows; i++) {
+            slotsList.add(slotsPerRow * (i - 1));
+            slotsList.add((slotsPerRow * (i - 1)) + (slotsPerRow - 1));
+        }
+        for (int i = 1; i <= (slotsPerRow - 2); i++) {
+            slotsList.add(i);
+        }
+        for (int i = slots - 2; i <= ((rows - 1) * slotsPerRow) + 1; i++) {
+            slotsList.add(i);
+        }
+        for (int i = (inv.getSize() - 1); i >= (inv.getSize() - slotsPerRow); i--) {
+            slotsList.add(i);
+        }
+        return slotsList.toArray(new Integer[0]);
     }
 
     @Override
---
+++ src/main/java/com/ankoki/olysene/utils/annotations/Builder.java
@@ -0,0 +1,13 @@
+package com.ankoki.olysene.utils.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Builder {
+    //T void builder();
+}
